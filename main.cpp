c9a0593182def2948b437ae8c3114919220b8238
EdgarEspinozaPE <64268406+EdgarEspinozaPE@users.noreply.github.com>
2020-06-02 15:43:49 -0500

    Lanzador del juego

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <SFML/Graphics.hpp>
-
-int main()
-{
+#include "juego.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include "TileMap.h"
+/*int main(){
+    
     sf::RenderWindow window(sf::VideoMode(1920,1080), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
     while (window.isOpen())
     {
         sf::Event event;
@@ -14,11 +17,16 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         window.clear();
         window.draw(shape);
         window.display();
     }
 
     return 0;
+}
+*/
+int main()
+{
+    juego* Arcanus;
+    Arcanus = new juego(800, 600, "Pruebas de Tilemap");
 }
